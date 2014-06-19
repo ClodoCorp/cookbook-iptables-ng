d0161dea70ad244cbd8a8d35db326e23656e3e5e
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-06-19 21:44:18 +0400

    fix empty data_bags
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- recipes/data_bag.rb
+++ recipes/data_bag.rb
@@ -2,28 +2,30 @@ rules = Mash.new
 sets = Hash.new
 bag = node['iptables-ng']['data_bag']
 
-node["iptables-ng"]["data_bags"].each do |item|
-  bag_item  = begin
-    if node["iptables-ng"]["secret"]
-      secret = Chef::EncryptedDataBagItem.load_secret(node["iptables-ng"]["secret"])
-      Chef::EncryptedDataBagItem.load(bag, item, secret)
-    else
-      data_bag_item(bag, item)
+unless node["iptables-ng"]["data_bags"].nil?
+  node["iptables-ng"]["data_bags"].each do |item|
+    bag_item  = begin
+      if node["iptables-ng"]["secret"]
+        secret = Chef::EncryptedDataBagItem.load_secret(node["iptables-ng"]["secret"])
+        Chef::EncryptedDataBagItem.load(bag, item, secret)
+      else
+        data_bag_item(bag, item)
+      end
+    rescue => ex
+      Chef::Log.info("Data bag #{bag} not found (#{ex}), so skipping")
+      Hash.new
     end
-  rescue => ex
-    Chef::Log.info("Data bag #{bag} not found (#{ex}), so skipping")
-    Hash.new
-  end
 
-  rules = Chef::Mixin::DeepMerge.merge(rules, bag_item["rules"])
+    rules = Chef::Mixin::DeepMerge.merge(rules, bag_item["rules"])
 
-  next unless bag_item["sets"]
+    next unless bag_item["sets"]
   
-  bag_item["sets"].each do |s|
-    sets = Chef::Mixin::DeepMerge.merge(sets, s)
+    bag_item["sets"].each do |s|
+      sets = Chef::Mixin::DeepMerge.merge(sets, s)
+    end
   end
-end
 
-node.set['iptables-ng']['sets'] = sets
-node.set['iptables-ng']['rules'] = rules
-include_recipe 'iptables-ng'
+  node.set['iptables-ng']['sets'] = sets
+  node.set['iptables-ng']['rules'] = rules
+  include_recipe 'iptables-ng'
+end
